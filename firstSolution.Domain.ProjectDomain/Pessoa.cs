31cfc98e38d643e4ad7d39c13c0ee601d62d9ebb
adamsinaga <38689336+adamsinaga@users.noreply.github.com>
2023-04-15 23:35:25 -0400

    Add files via upload

--- firstSolution.Domain.ProjectDomain/Pessoa.cs
+++ firstSolution.Domain.ProjectDomain/Pessoa.cs
@@ -1,34 +1,42 @@
-﻿namespace firstSolution.Domain.ProjectDomain
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace firstSolution.Domain.ProjectDomain
 {
-    public class Pessoa
+    public class Pessoa 
     {
         public int PessoaId { get; set; } //propiedade PessoaId
         public String Nome { get; set; } //propriedade nome
 
         public DateTime DataNascimento { get; set; }
 
-        public class PessoaRepository
+        public class PessoaRepository //gerenciar uma lista de objetos "Pessoa".
         {
-            private List<Pessoa> pessoas = new List<Pessoa>();
+            //cria uma lista vazia de pessoas
+            private List<Pessoa> pessoas = new List<Pessoa>(); 
 
             public void Adicionar(Pessoa pessoa)
             {
-                pessoas.Add(pessoa);
+                // Adiciona um objeto "Pessoa" à lista.
+                pessoas.Add(pessoa); 
             }
 
             public void Remover(Pessoa pessoa)
             {
-                pessoas.Remove(pessoa);
+                // Remove um objeto "Pessoa" da lista.
+                pessoas.Remove(pessoa);  
             }
 
             public List<Pessoa> BuscarPorNome(string nome)
             {
-                return pessoas.Where(p => p.Nome == nome).ToList();
+                // Retorna uma lista de objetos "Pessoa" que têm um nome correspondente ao parâmetro de entrada.
+                return pessoas.Where(p => p.Nome == nome).ToList(); 
+
             }
 
             public void Atualizar(Pessoa pessoa)
             {
-                int index = pessoas.FindIndex(p => p.PessoaId == pessoa.PessoaId);
+                // Atualiza um objeto "Pessoa" existente na lista com as informações do objeto "Pessoa" passado como parâmetro
+                int index = pessoas.FindIndex(p => p.PessoaId == pessoa.PessoaId); 
                 if (index != -1)
                 {
                     pessoas[index] = pessoa;
@@ -37,3 +45,4 @@
         }
 
     }
+}
